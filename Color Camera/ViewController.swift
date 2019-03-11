1bcb8d561382398468e5340ca162956339237a6e
Ted Spikes <ted@tedspikes.com>
2019-03-11 13:36:22 +0200

    Initial Commit

---
+++ Color Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Camera
+//
+//  Created by Fedor on 3/11/19.
+//  Copyright © 2019 Fedor Kostylev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
