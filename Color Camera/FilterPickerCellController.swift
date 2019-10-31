cf486c9e12919d0435882b39f8979133f05a1a21
Ted Spikes <ted@tedspikes.com>
2019-10-31 15:08:09 +0200

    Added custom CellView nib and class. Removed transparency switch.

---
+++ Color Camera/FilterPickerCellController.swift
@@ -0,0 +1,15 @@
+//
+//  FilterPickerCellController.swift
+//  Color Camera
+//
+//  Created by Fedor on 10/31/19.
+//  Copyright © 2019 Fedor Kostylev. All rights reserved.
+//
+
+import UIKit
+
+class FilterPickerCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
--- Color Camera/FilterPickerViewController.swift
+++ Color Camera/FilterPickerViewController.swift
@@ -9,51 +9,29 @@
 import UIKit
 
 class FilterPickerViewController: UIViewController {
-    let filters:[String] = [
-        "Normal",
-        "Protanopia",
-        "Protanomaly",
-        "Deuteranopia",
-        "Deuteranomaly",
-        "Tritanopia",
-        "Tritanomaly",
-        "Achromatopsia",
-        "Achromatomaly"
+    let filters:[(String, String)] = [
+        ( "Normal", "As the camera sees it." ),
+        ( "Protanopia", "Full red-blindness, missing L-cones." ),
+        ( "Protanomaly", "Red-blindness, defective L-cones." ),
+        ( "Deuteranopia", "Full green-blindness." ),
+        ( "Deuteranomaly", "Varies per person, green sensitivity is moved to red." ),
+        ( "Tritanopia", "Rare form of missing S-cones, blue can be confused with green and yellow with violet." ),
+        ( "Tritanomaly", "Usually, a lighter form of Tritanopia." ),
+        ( "Achromatopsia", "Total color blindness." ),
+        ( "Achromatomaly", "An alleviated form of Achromatopsia" )
     ]
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var transparentSwitch: UISwitch!
     @IBOutlet weak var navBar: UINavigationBar!
     
     @IBAction func closeModal(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func toggleTransparent(_ sender: Any) {
-        if self.transparentSwitch.isOn {
-            self.view.backgroundColor = UIColor.clear
-            self.tableView.backgroundColor = UIColor.clear
-            self.navBar.backgroundColor = UIColor.clear
-            self.navBar.barTintColor = UIColor.clear
-            self.navBar.isTranslucent = true
-            for cell in self.tableView.visibleCells {
-                cell.backgroundColor = UIColor.clear
-            }
-        } else {
-            self.view.backgroundColor = UIColor.white
-            self.tableView.backgroundColor = UIColor.white
-            self.navBar.backgroundColor = UIColor.white
-            self.navBar.barTintColor = UIColor.white
-            self.navBar.isTranslucent = true
-            for cell in self.tableView.visibleCells {
-                cell.backgroundColor = UIColor.white
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.tableView.register(UINib.init(nibName: "FilterPickerCellView", bundle: nil), forCellReuseIdentifier: "filterPickerCell")
         self.tableView.delegate = self
         self.tableView.dataSource = self
     }
@@ -79,9 +57,10 @@ extension FilterPickerViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "FilterPickerCell")
-        cell.textLabel?.text = self.filters[indexPath.row]
-        cell.detailTextLabel?.text = "Detail"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "filterPickerCell") as! FilterPickerCell
+        let filterDescription = self.filters[indexPath.row]
+        cell.nameLabel?.text = filterDescription.0
+        cell.descriptionLabel?.text = filterDescription.1
         
         return cell
     }
